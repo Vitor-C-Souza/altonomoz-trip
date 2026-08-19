db69343a8edbb3aa0e982977cc71ea9401e25683
Vítor Cavalcante Souza <51930276+Vitor-C-Souza@users.noreply.github.com>
2026-08-19 18:28:24 -0300

    initialize Spring Boot application with Maven setup and basic configuration

---
+++ src/main/java/app/vitorcsouza/altonomoz_trip/AltonomozTripApplication.java
@@ -0,0 +1,13 @@
+package app.vitorcsouza.altonomoz_trip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AltonomozTripApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AltonomozTripApplication.class, args);
+	}
+
+}
---
+++ src/test/java/app/vitorcsouza/altonomoz_trip/AltonomozTripApplicationTests.java
@@ -0,0 +1,13 @@
+package app.vitorcsouza.altonomoz_trip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AltonomozTripApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
