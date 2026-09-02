bd6726e5329ffd481f2d574480de476d71001834
Vítor Cavalcante Souza <51930276+Vitor-C-Souza@users.noreply.github.com>
2026-09-02 12:04:49 -0300

    fix: calculate derived trip values before persistence

--- src/main/java/app/vitorcsouza/altonomoz_trip/application/service/CreateTripService.java
+++ src/main/java/app/vitorcsouza/altonomoz_trip/application/service/CreateTripService.java
@@ -3,6 +3,7 @@ package app.vitorcsouza.altonomoz_trip.application.service;
 import app.vitorcsouza.altonomoz_trip.application.port.in.CreateTripUseCase;
 import app.vitorcsouza.altonomoz_trip.application.port.out.TripRepositoryPort;
 import app.vitorcsouza.altonomoz_trip.domain.model.Trip;
+import app.vitorcsouza.altonomoz_trip.infrastructure.exception.TripAlreadyExistsException;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
@@ -13,8 +14,10 @@ public class CreateTripService implements CreateTripUseCase {
     @Override
     public Trip execute(Trip trip) {
         if (tripRepositoryPort.existsByOs(trip.getOs())) {
-            throw new IllegalArgumentException("Já existe uma viagem registrada com a OS: " + trip.getOs());
+            throw new TripAlreadyExistsException(trip.getOs());
         }
+
+        trip.calcularValoresDerivados();
         return tripRepositoryPort.save(trip);
     }
 }
