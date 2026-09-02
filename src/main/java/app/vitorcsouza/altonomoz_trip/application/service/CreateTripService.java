ccf3ec5d3492f93e3ce17c9020d30363c22f351d
Vítor Cavalcante Souza <51930276+Vitor-C-Souza@users.noreply.github.com>
2026-09-02 12:06:05 -0300

    refactor: move duplicate trip exception to application layer

--- src/main/java/app/vitorcsouza/altonomoz_trip/application/service/CreateTripService.java
+++ src/main/java/app/vitorcsouza/altonomoz_trip/application/service/CreateTripService.java
@@ -1,9 +1,9 @@
 package app.vitorcsouza.altonomoz_trip.application.service;
 
+import app.vitorcsouza.altonomoz_trip.application.exception.TripAlreadyExistsException;
 import app.vitorcsouza.altonomoz_trip.application.port.in.CreateTripUseCase;
 import app.vitorcsouza.altonomoz_trip.application.port.out.TripRepositoryPort;
 import app.vitorcsouza.altonomoz_trip.domain.model.Trip;
-import app.vitorcsouza.altonomoz_trip.infrastructure.exception.TripAlreadyExistsException;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
