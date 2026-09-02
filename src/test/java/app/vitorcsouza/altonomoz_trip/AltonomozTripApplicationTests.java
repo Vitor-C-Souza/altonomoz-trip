a01237ba7ad7ad9096116390d274c8e01a4e0663
Vítor Cavalcante Souza <51930276+Vitor-C-Souza@users.noreply.github.com>
2026-09-02 12:51:37 -0300

    test: isolate application context from external services

--- src/test/java/app/vitorcsouza/altonomoz_trip/AltonomozTripApplicationTests.java
+++ src/test/java/app/vitorcsouza/altonomoz_trip/AltonomozTripApplicationTests.java
@@ -3,11 +3,14 @@ package app.vitorcsouza.altonomoz_trip;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = {
+        "CSV_URL=",
+        "spring.mongodb.uri=mongodb://localhost:27017/altonomoz-trip-test"
+})
 class AltonomozTripApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
